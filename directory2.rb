48b36181d8822e399c22174975dcb05196cccad8
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-09-08 16:34:43 +0100

    directory duplicate exercise

--- directory2.rb
+++ directory2.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, number|
+    puts "#{number + 1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
